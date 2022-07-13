09dd6c605e42f231c874c85e17e40a57bb698e84
Sofie Henriksen <sofie.henriksen@itslearning.com>
2022-07-13 15:15:07 +0200

    Misc

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-- You will need to have Node installed to start this environment. If you are not sure if you have Node installed run `node -v` in your terminal. If you do not see a version number output, install Node before moving on.
-- Install light-server locally and save it to your development environment by running `npm install lite-server --save-dev` in your terminal.
-- Run `npm run start` in your terminal to start the server.
-- Your browser will open at `localhost:3000`.
+# Installation Instructions
+## Twitter Data
+* Create a new folder in `twtdata` and set the name to be the same as your twitter username.
+* Get your twitter archive from https://twitter.com/settings/download_your_data. The archive is ready to download after 24 hours.
+* Extract `tweet.js` and `like.js` from the archive zip file into `twtdata\`__*`username`*__.
+* Rename `window.YTD.tweet.part0` in `twtdata\`__*`username`*__`\tweet.js` to __*`username`*__`_tweet`.
+* Rename `window.YTD.like.part0` in `twtdata\`__*`username`*__`\like.js` to __*`username`*__`_like`.
+* Import `tweet.js` and `like.js` to `index.html` after `<div id="results-container"></div>` as:  
+`<script src="/twtdata/`__*`username`*__`/like.js"></script>`  
+`<script src="/twtdata/`__*`username`*__`/tweet.js"></script>`
+* Get your account I.D. at https://tweeterid.com/.
+* Fill in `const archives = {}` in `script.js` in the following format:  
+`"`__*`I.D.`*__`-tweet": `__*`username`*__`_tweet`  
+`"`__*`I.D.`*__`-like": `__*`username`*__`_like`
+* Fill in `<select name="search-option" id="search-option"></select>` in `index.html` in the following format:  
+`<option value="`__*`I.D.`*__`-tweet">`__*`username`*__`'s tweets</option>`  
+`<option value="`__*`I.D.`*__`-like">`__*`username`*__`'s likes</option>`
+
+## Node
+* You will need to have Node installed to start this environment. If you are not sure if you have Node installed run `node -v` in your terminal. If you do not see a version number output, install Node before moving on.
+* Install light-server locally and save it to your development environment by running `npm install lite-server --save-dev` in your terminal.
+* Run `npm run start` in your terminal to start the server.
+* Your browser will open at `localhost:3000`.
---
+++ filters.js
@@ -0,0 +1,104 @@
+function setFilterRules(filter1, filter2) {
+    Array.from(document.getElementsByClassName(filter1)).forEach((filter) => {
+        const otherFilter = document.getElementsByClassName(filter2)[filter.name];
+        if (filter.checked) {
+            otherFilter.disabled = false;
+            otherFilter.checked = false;
+            otherFilter.disabled = true;
+        }
+        filter.addEventListener("change", function () {
+            otherFilter.disabled = !otherFilter.disabled;
+        });
+    });
+}
+setFilterRules("inc-filter", "exc-filter");
+setFilterRules("exc-filter", "inc-filter");
+
+function retweetFilter(tweet) {
+    if (tweet.tweet.full_text.startsWith("RT @") && tweet.tweet.entities.user_mentions.length === 1) {
+        let screenName = tweet.tweet.full_text.substring(4).split(": ")[0];
+        for (let index = 0; index < tweet.tweet.entities.user_mentions.length; index++) {
+            if (tweet.tweet.entities.user_mentions[index].screen_name === screenName) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function replyFilter(tweet) {
+    return typeof tweet.tweet.in_reply_to_status_id_str !== "undefined";
+}
+
+function threadFilter(tweet) {
+    const searchOption = document.getElementById("search-option").value;
+    if (tweet.tweet.in_reply_to_user_id_str === searchOption.substring(0, searchOption.length - 6)) {
+        return true;
+    }
+    return false;
+}
+
+function mediaFilter(tweet) {
+    return typeof tweet.tweet.entities.media !== "undefined";
+}
+
+function spotifyFilter(tweet) {
+    for (let index = 0; index < tweet.tweet.entities.urls.length; index++) {
+        if (tweet.tweet.entities.urls[index].expanded_url.startsWith("https://open.spotify.com")) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function youtubeFilter(tweet) {
+    for (let index = 0; index < tweet.tweet.entities.urls.length; index++) {
+        if (tweet.tweet.entities.urls[index].expanded_url.startsWith("https://www.youtube.com")) {
+            return true;
+        }
+    }
+    return false;
+}
+
+const filters = [
+    function (tweet) {return retweetFilter(tweet);},
+    function (tweet) {return replyFilter(tweet);},
+    function (tweet) {return threadFilter(tweet);},
+    function (tweet) {return mediaFilter(tweet);},
+    function (tweet) {return spotifyFilter(tweet);},
+    function (tweet) {return youtubeFilter(tweet);},
+];
+
+function filterResult(tweet) {
+    let filterOK;
+    console.log("LOLLL");
+    const incFilters = Array.from(document.getElementsByClassName("inc-filter"));
+    if (document.getElementById("inc-filter-and").checked) {
+        filterOK = true;
+        for (i = 0; i < filters.length && i < incFilters.length && filterOK; i++) {
+            console.log(incFilters[i].id + ": " + incFilters[i].checked);
+            filterOK = filterOK && (!incFilters[i].checked || filters[i](tweet));
+        }
+    } else if (document.getElementById("inc-filter-or").checked) {
+        filterOK = false;
+        for (i = 0; i < filters.length && i < incFilters.length && !filterOK; i++) {
+            console.log(incFilters[i].id + ": " + incFilters[i].checked);
+            filterOK = filterOK || (!incFilters[i].checked || filters[i](tweet));
+        }
+    }
+    if (filterOK) {
+        const excFilters = Array.from(document.getElementsByClassName("exc-filter"));
+        if (document.getElementById("exc-filter-and").checked) {
+            for (i = 0; i < filters.length && i < excFilters.length && filterOK; i++) {
+                console.log(excFilters[i].id + ": " + excFilters[i].checked);
+                filterOK = filterOK && (!excFilters[i].checked && !filters[i](tweet));
+            }
+        } else if (document.getElementById("exc-filter-or").checked) {
+            for (i = 0; i < filters.length && i < excFilters.length && !filterOK; i++) {
+                console.log(excFilters[i].id + ": " + excFilters[i].checked);
+                filterOK = filterOK || !(excFilters[i].checked && filters[i](tweet));
+            }
+        }
+    }
+    return filterOK;
+}
